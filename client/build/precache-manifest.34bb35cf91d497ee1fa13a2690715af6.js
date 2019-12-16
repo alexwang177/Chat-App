007879d41472fe3d1f5fcf16f946a46de98b38f6
Alex Wang <jianuo.wang@duke.edu>
2019-12-15 22:15:47 -0600

    build

---
+++ client/build/precache-manifest.34bb35cf91d497ee1fa13a2690715af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45a47258fadae27d14050d9ee228448a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac45ce4dcaeb25e463ff",
+    "url": "/static/css/main.30cc34ed.chunk.css"
+  },
+  {
+    "revision": "c504f6e5e4fef2a935a3",
+    "url": "/static/js/2.3bc32740.chunk.js"
+  },
+  {
+    "revision": "e5fdd3c10a39c54f9723efcb1214ee6a",
+    "url": "/static/js/2.3bc32740.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ac45ce4dcaeb25e463ff",
+    "url": "/static/js/main.bc146edc.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
