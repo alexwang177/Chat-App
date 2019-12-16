224af5233d739046294cd8ee59f02deda8e29582
Alex Wang <jianuo.wang@duke.edu>
2019-12-15 21:59:24 -0600

    change endpoint

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 let socket;
 
 const Chat = ({ location }) => {
     const[name, setName] = useState('');
     const[room, setRoom] = useState('');
-    const ENDPOINT = 'localhost:5000';
+    const[messages, setMessages] = useState([]);
+    const[message, setMessage] = useState('');
+    const ENDPOINT = 'https://chat-app-alex.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
@@ -28,8 +34,32 @@ const Chat = ({ location }) => {
 
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        })
+    }, [messages]);
+
+    // function for sending messages
+    const sendMessage = (event) => {
+
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    } 
+
+    console.log(message, messages);
+
     return(
-        <h1>Chat</h1>
+        <div className='outerContainer'>
+            <div className='container'>
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+            </div>
+        </div>
     )
 }
 
